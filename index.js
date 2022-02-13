ac7bdced676ff37879914e3b7da443d9ad829c38
Nikita <nikita.maximtsev@gmail.com>
2022-02-13 08:42:08 +0300

    Nikita's refactoring

--- index.js
+++ index.js
@@ -1,87 +1,62 @@
-const tasksList = [ 
-  { id: "1", text: "выучить html", completed: true }, 
-  { id: "2", text: "выучить css", completed: true }, 
-  { id: "3", text: "выучить js", completed: false }, 
-  { id: "4", text: "выучить фреймворк", completed: false }, 
-  { id: "5", text: "написать несколько учебных проектов", completed: false }, 
-  { id: "6", text: "пройти собеседование", completed: false }, 
-  { id: "7", text: "получить работу", completed: false }, 
-  ]; 
-const ul = document.querySelector('.todo-list') 
- 
- 
-function getId(){ 
-  let idsArray = tasksList.map(i => i.id) 
-  console.log(idsArray) 
-  let maxId = Math.max(...idsArray); 
-} 
-getId(); 
- 
- 
-function renderTask (task){  
- 
-  const listItem = document.createElement('li'); 
- 
-  const div = document.createElement('div'); 
-  div.className = 'view'; 
- 
-  const input = document.createElement('input'); 
-  input.className = "toggle";  
-  input.type = "checkbox"; 
-  input.checked = task.completed 
-   
-  const label = document.createElement('label'); 
-  label.innerHTML = task.text 
- 
- 
-  const button = document.createElement('button'); 
-  button.className = "destroy"; 
- 
- 
-  ul.appendChild(listItem) 
-  listItem.appendChild(div) 
-  div.appendChild(input) 
-  div.appendChild(label) 
-  div.appendChild(button) 
- 
-  console.log(ul) 
-} 
- 
-tasksList.forEach(function(task) { 
-renderTask(task); 
-}); 
- 
-(function createNewTask() { 
-  const input = document.querySelector('.new-todo').addEventListener('keydown', function(e) { 
-    if (e.keyCode === 13 && this.value != '') { 
-      tasksList.push({ id:String.newId, text: this.value, completed: false },); 
-      let newTask = tasksList[tasksList.length - 1] 
-      
-        const listItem = document.createElement('li'); 
-     
-        const div = document.createElement('div'); 
-        div.className = 'view'; 
-     
-        const input = document.createElement('input'); 
-        input.className = "toggle";  
-        input.type = "checkbox"; 
-        input.checked = newTask.completed 
-         
-        const label = document.createElement('label'); 
-        label.innerHTML = newTask.text 
-     
-     
-        const button = document.createElement('button'); 
-        button.className = "destroy"; 
-     
-     
-        ul.appendChild(listItem) 
-        listItem.appendChild(div) 
-        div.appendChild(input) 
-        div.appendChild(label) 
-        div.appendChild(button) 
-     
-        console.log(ul) 
- 
-        this.value = ''; 
-    }});})();
+const tasksList = [
+  { id: "1", text: "выучить html", completed: true },
+  { id: "2", text: "выучить css", completed: true },
+  { id: "3", text: "выучить js", completed: false },
+  { id: "4", text: "выучить фреймворк", completed: false },
+  { id: "5", text: "написать несколько учебных проектов", completed: false },
+  { id: "6", text: "пройти собеседование", completed: false },
+  { id: "7", text: "получить работу", completed: false },
+];
+const ul = document.querySelector('.todo-list');
+const mainInput = document.querySelector('.new-todo');
+
+renderTasks(tasksList);
+
+function getId() {
+  let idsArray = tasksList.map(i => i.id)
+  console.log(idsArray)
+  let maxId = Math.max(...idsArray);
+  return maxId + 1;
+}
+
+mainInput.addEventListener('keydown', function (e) {
+  const enterKeyCode = 13;
+  if (e.keyCode === enterKeyCode && e.target.value !== '') {
+    const newTaskId = getId();
+    const newTask = { id: newTaskId, text: this.value, completed: false };
+    tasksList.push(newTask);
+    renderTasks(tasksList);
+    e.target.value = '';
+  }
+})
+
+function renderTasks(tasks) {
+  ul.innerHTML = '';
+  tasks.forEach(task => {
+    renderTask(task);
+  })
+}
+
+function renderTask(task) {
+  const listItem = document.createElement('li');
+
+  const div = document.createElement('div');
+  div.className = 'view';
+
+  const input = document.createElement('input');
+  input.className = "toggle";
+  input.type = "checkbox";
+  input.checked = task.completed;
+
+  const label = document.createElement('label');
+  label.innerHTML = task.text;
+
+  const button = document.createElement('button');
+  button.className = "destroy";
+
+  ul.appendChild(listItem);
+  listItem.appendChild(div);
+  div.appendChild(input);
+  div.appendChild(label);
+  div.appendChild(button);
+}
